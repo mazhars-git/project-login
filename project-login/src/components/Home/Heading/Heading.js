d7cadaa2f83dd9f9006295137f59aa333ef1e001
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-08-24 23:31:24 +0600

    update code

--- project-login/src/components/Home/Heading/Heading.js
+++ project-login/src/components/Home/Heading/Heading.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import '../Css/Heading.css';
 
 const Heading = () => {
     return (
-        <section className="col-md-4 bg-primary text-white">
-            <div className="align-self-center">
+        <section className="col-md-4 bg-primary text-white heading-area">
+            <div className="w-75 heading-content">
                 <h3>Dummy Heading</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                
             </div>
         </section>
     );
+
 };
 
 export default Heading;
--- project-login/src/components/Home/Login/Login.js
+++ project-login/src/components/Home/Login/Login.js
@@ -1,28 +1,46 @@
 import React from 'react';
 
 const Login = () => {
+    const handleBlur = (e) => {
+        console.log(e.target.name, e.target.value)
+        if (e.target.name === "email") {
+            const isValidEmail = /\S+@\S+\.\S+/.test(e.target.value);
+            console.log(isValidEmail);
+        }
+        if (e.target.name === "password") {
+            const isValidPass = e.target.value.length > 8;
+            console.log(isValidPass); 
+        }
+    }
+
+    const handleSubmit = () =>{
+
+    }
+
     return (
         <section className="col-md-8">
-            <div className="m-5">
+            <div className="mt-5 w-50 m-auto">
                 <h3>Let's set up your account</h3>
-                <p>Already have an account? Sign in</p>
-                <form>
-                <input className="form-control mb-3" type="text" placeholder="Your name" required/>
-                <input className="form-control mb-3" type="email" placeholder="Email address" required/>
-                <input className="form-control mb-3" list="datalistOptions" placeholder="I would describe my user type as"/>
-                    <datalist id="datalistOptions">
-                        <option value="Web Designer"/>
-                        <option value="Web Developer"/>
-                        <option value="Digital Marketer"/>
-                        <option value="SEO Specialist"/>
-                        <option value="Graphic Designer"/>
-                    </datalist>
-                <input className="form-control mb-3" type="password" placeholder="Password" />
-                <span>Minimum 8 characters</span> <br/>
+                <p>Already have an account? <a href="#">Sign in</a></p>
 
-                <button type="submit" className="btn btn-primary my-3">Sign in</button>
+                <form onSubmit={handleSubmit}>
+                    <input name="name" onBlur={handleBlur} className="form-control mb-3" type="text" placeholder="Your name" required/>
+                    <input name="email" onBlur={handleBlur} className="form-control mb-3" type="email" placeholder="Email address" required/>
+                    <input name="type" onBlur={handleBlur} className="form-control mb-3" list="datalistOptions" placeholder="I would describe my user type as"/>
+                        <datalist id="datalistOptions">
+                            <option value="Web Designer"/>
+                            <option value="Web Developer"/>
+                            <option value="Digital Marketer"/>
+                            <option value="SEO Specialist"/>
+                            <option value="Graphic Designer"/>
+                        </datalist>
+                    <input name="password" onBlur={handleBlur} className="form-control mb-3" type="password" placeholder="Password" />
+                    <span>Minimum 8 characters</span> <br/>
 
+                    <input type="submit" className="form-control btn btn-primary my-3" value="Next"/>
                 </form>
+
+                <p>By clicking the "Next" button, you agree to creating a free account, and to <a href="#">Terms of Service</a> and <a href="#">Privacy Policy.</a></p>
             </div>
         </section>
     );
