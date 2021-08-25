5b63463b98e4d7c155f2f792e18515f14a9fa4d6
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-08-25 17:40:59 +0600

    update

--- project-login/src/components/Home/Heading/Heading.js
+++ project-login/src/components/Home/Heading/Heading.js
@@ -7,7 +7,6 @@ const Heading = () => {
             <div className="w-75 heading-content">
                 <h3>Dummy Heading</h3>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-                
             </div>
         </section>
     );
--- project-login/src/components/Home/Login/Login.js
+++ project-login/src/components/Home/Login/Login.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import { ToggleButton } from 'react-bootstrap';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 const Login = () => {
     const [user, setUser] = useState({
@@ -28,11 +27,10 @@ const Login = () => {
         }
     }
 
-    const handleSubmit = (e) =>{
+    const handleSubmit = () =>{
         if (user.email && user.password) {
             console.log("submitting....")
         }
-        e.preventDefault();
     }
 
     return (
@@ -40,10 +38,6 @@ const Login = () => {
             <div className="mt-5 w-50 m-auto">
                 <h3>Let's set up your account</h3>
                 <p>Already have an account? <a href="#">Sign in</a></p>
-
-                {/* <p>Name: {user.name}</p>
-                <p>Email: {user.email}</p>
-                <p>Pass: {user.password}</p> */}
                 
                 <form onSubmit={handleSubmit}>
                     <input name="name" onBlur={handleBlur} className="form-control mb-3" type="text" placeholder="Your name" required/>
@@ -62,7 +56,8 @@ const Login = () => {
                             <option value="SEO Specialist"/>
                             <option value="Graphic Designer"/>
                         </datalist>
-                    <input name="password" onBlur={handleBlur} className="form-control mb-3" type="password" placeholder="Password" />
+                    <input name="password" onBlur={handleBlur} className="form-control mb-3 pass-icon" type="password" placeholder="Password" />
+                    <FontAwesomeIcon icon={faEye} className="eye"/>
                     
                     {user.password?
                         <span>Minimum 8 characters</span>
@@ -75,7 +70,6 @@ const Login = () => {
                         : <input type="submit" className="form-control btn btn-primary my-3 btn-disabled" value="Next"/>
                     }
                 </form>
-
                 <p>By clicking the "Next" button, you agree to creating a free account, and to <a href="#">Terms of Service</a> and <a href="#">Privacy Policy.</a></p>
             </div>
         </section>
