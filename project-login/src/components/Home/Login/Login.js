1de16fa90eae7c4bff2461696569835d8912d4f9
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-08-25 16:59:30 +0600

    update

--- project-login/src/components/Home/Login/Login.js
+++ project-login/src/components/Home/Login/Login.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { ToggleButton } from 'react-bootstrap';
 
 const Login = () => {
+    const [user, setUser] = useState({
+        name: '',
+        email : '',
+        password: ''
+    });
+
     const handleBlur = (e) => {
         console.log(e.target.name, e.target.value)
+            let isValidForm = true;
         if (e.target.name === "email") {
-            const isValidEmail = /\S+@\S+\.\S+/.test(e.target.value);
-            console.log(isValidEmail);
+            const isEmailValid = /\S+@\S+\.\S+/.test(e.target.value);
+            isValidForm = isEmailValid;
         }
         if (e.target.name === "password") {
-            const isValidPass = e.target.value.length > 8;
-            console.log(isValidPass); 
+            const isPassValid = e.target.value.length > 8;
+            isValidForm = isPassValid;
+        }
+        if (isValidForm) {
+            const newUserInfo = {...user};
+            newUserInfo[e.target.name] = e.target.value;
+            setUser(newUserInfo);
         }
     }
 
-    const handleSubmit = () =>{
-
+    const handleSubmit = (e) =>{
+        if (user.email && user.password) {
+            console.log("submitting....")
+        }
+        e.preventDefault();
     }
 
     return (
@@ -23,9 +41,19 @@ const Login = () => {
                 <h3>Let's set up your account</h3>
                 <p>Already have an account? <a href="#">Sign in</a></p>
 
+                {/* <p>Name: {user.name}</p>
+                <p>Email: {user.email}</p>
+                <p>Pass: {user.password}</p> */}
+                
                 <form onSubmit={handleSubmit}>
                     <input name="name" onBlur={handleBlur} className="form-control mb-3" type="text" placeholder="Your name" required/>
                     <input name="email" onBlur={handleBlur} className="form-control mb-3" type="email" placeholder="Email address" required/>
+                    
+                   { user.email?
+                      ' '
+                    : <p style={{color: 'red'}}>Please enter a valid email address</p>
+                   }
+
                     <input name="type" onBlur={handleBlur} className="form-control mb-3" list="datalistOptions" placeholder="I would describe my user type as"/>
                         <datalist id="datalistOptions">
                             <option value="Web Designer"/>
@@ -35,9 +63,17 @@ const Login = () => {
                             <option value="Graphic Designer"/>
                         </datalist>
                     <input name="password" onBlur={handleBlur} className="form-control mb-3" type="password" placeholder="Password" />
-                    <span>Minimum 8 characters</span> <br/>
+                    
+                    {user.password?
+                        <span>Minimum 8 characters</span>
+                        : <span style={{color: 'red'}}>Minimum 8 characters</span>
+                    } <br/>
 
-                    <input type="submit" className="form-control btn btn-primary my-3" value="Next"/>
+                    {user.email && user.password?
+                          <input type="submit" className="form-control btn btn-primary my-3" value="Next" />
+
+                        : <input type="submit" className="form-control btn btn-primary my-3 btn-disabled" value="Next"/>
+                    }
                 </form>
 
                 <p>By clicking the "Next" button, you agree to creating a free account, and to <a href="#">Terms of Service</a> and <a href="#">Privacy Policy.</a></p>
