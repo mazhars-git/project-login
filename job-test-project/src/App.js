c601c28fcf487642dc7cd6b356251b9a79ea8734
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-01-09 20:13:11 +0600

    first

---
+++ job-test-project/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+  );
+}
+
+export default App;
